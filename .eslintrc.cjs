8eead956a6f8c1c43ef6553c38fdf26c8ce7884d
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-08-27 00:06:20 +0900

    Initial project

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export default function App (): JSX.Element {
+  return (
+    <div>hello</div>
+  )
+}
